eb46e23f41f2dd14f33e0707b7ac7b3328a43201
Tomasz Urbaniak <tomekgnu@gmail.com>
2020-06-29 21:02:52 +0200

    Adding directfb stuff

--- Debug/src/main.d
+++ Debug/src/main.d
@@ -1,8 +1,112 @@
-src/main.o: ../src/main.c /home/nanker/workspace/KetronBPI/include/main.h \
+src/main.o: ../src/main.c \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/clock.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/clock.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/types.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/build.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/linux/glibc/types.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/types.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/result.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/debug.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/conf.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/log_domain.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/compiler.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/log.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/log.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/mutex.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/linux/glibc/mutex.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/util.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/messages.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/print.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/system.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/system.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/thread.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/thread.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/linux/glibc/thread.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/waitqueue.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/linux/glibc/waitqueue.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/linux/glibc/mutex.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/trace.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/include/directfb.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/include/dfb_types.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/include/directfb_build.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/include/directfb_keyboard.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/interface.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/mem.h \
+ /home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/mem.h \
+ /home/nanker/workspace/KetronBPI/include/main.h \
  /home/nanker/workspace/KetronBPI/include/midi.h \
  /home/nanker/workspace/KetronBPI/include/MD_MIDIFile.h \
  /home/nanker/workspace/KetronBPI/include/main.h \
- /home/nanker/workspace/KetronBPI/include/controls.h
+ /home/nanker/workspace/KetronBPI/include/controls.h \
+ /home/nanker/workspace/fbg/src/fbgraphics.h
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/clock.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/clock.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/types.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/build.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/linux/glibc/types.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/types.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/result.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/debug.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/conf.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/log_domain.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/compiler.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/log.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/log.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/mutex.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/linux/glibc/mutex.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/util.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/messages.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/print.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/system.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/system.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/thread.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/thread.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/linux/glibc/thread.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/waitqueue.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/linux/glibc/waitqueue.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/linux/glibc/mutex.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/trace.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/include/directfb.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/include/dfb_types.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/include/directfb_build.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/include/directfb_keyboard.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/interface.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/mem.h:
+
+/home/nanker/Embedded/buildroot/output/build/directfb-1.7.7/lib/direct/os/mem.h:
 
 /home/nanker/workspace/KetronBPI/include/main.h:
 
@@ -13,3 +117,5 @@ src/main.o: ../src/main.c /home/nanker/workspace/KetronBPI/include/main.h \
 /home/nanker/workspace/KetronBPI/include/main.h:
 
 /home/nanker/workspace/KetronBPI/include/controls.h:
+
+/home/nanker/workspace/fbg/src/fbgraphics.h:
--- src/main.c
+++ src/main.c
@@ -6,70 +6,86 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <string.h>
+#include <direct/clock.h>
+#include <direct/debug.h>
+#include <direct/util.h>
+#include <directfb.h>
 #include "main.h"
 #include "midi.h"
 #include "controls.h"
+#include "fbgraphics.h"
 
-int com_serial;
-int failcount;
-extern void * bankArray[5];
+extern void *bankArray[5];
+int main2( int argc, char *argv[] );
 
-int main() {
-  int fduart,fdspi;
-  unsigned char data[6];
-  unsigned char byte,numOfBytes;
-  struct bank *currentBank = bankArray[bankA];
-  __useconds_t sleepTime = 1000000;
-  int joyx = 0,joyy = 0;
-  BOOL changed = FALSE;
-  //struct drumsets *drumsetPtr = bankArray[bankDrums];
+int keep_running = 1;
 
-  fduart = open("/dev/ttyS2", O_RDWR | O_NONBLOCK);
-  if (fduart <0) {perror("/dev/ttyS2"); exit(-1); }
-  fdspi = open("/dev/spimega328",O_RDONLY);
-  if (fdspi <0) {perror("/dev/spimega328"); exit(-1); }
-
-  midiInit();	// very important: MIDI_WAIT
-
-  /* family_data[family][sound][0] = bank;
-   * family_data[family][sound][1] = program number
-   */
-  sendProgramChange(fduart,currentBank->ID,currentBank->index + 1);
-  while (1){
-
-	  if(read(fduart, &byte, 1) == 1){
-		  if(readMidiMessage(byte,&numOfBytes) == TRUE && changed == FALSE){
-			 sendMidiMessage(fduart,numOfBytes);
-		  }
-	  }
-
-	  if(read(fdspi,data,6) > 0){
-		  translateJoystick(data[JOYX],data[JOYY],&joyx,&joyy);
-		  sleepTime = calculateSleepTime(joyx);
-		  changed = TRUE;
-		}
-	  	//printf("Button %d\n",c);
-
-	  else{
-		  //printf("%d\n",currentBank->index);
-		  if(joyx > 5 && currentBank->index < 127)
-			  currentBank->index++;
-		  else if(joyx < -5 && currentBank->index > 0)
-			  currentBank->index--;
-		  else changed = FALSE;
-		  if(changed == TRUE){
-			  printf("%d: %s\n",currentBank->index + 1,currentBank->names[currentBank->index]);
-			  sendProgramChange(fduart,currentBank->ID,currentBank->index);
-			  usleep(sleepTime);
-		  }
-
-	  }
-
-  }
-
-  close(fduart);
-
-  return 0;
+void int_handler(int dummy) {
+	keep_running = 0;
 }
 
+int main(int argc,char *argv[]) {
+
+	int fd_uart, fd_spi; /* file descriptors for UART-midi and spimega */
+	unsigned char inputdata[6]; /* 6 inputs from atmega */
+	unsigned char byte, numOfBytes; /* for UART-Midi communication */
+	struct bank *currentBank = bankArray[bankA]; /* Bank A selected initially */
+
+	__useconds_t sleepTime = 1000000;
+	int joyx = 0, joyy = 0; /* track joystick position: x,y coordinates zero-centered */
+	BOOL joychanged = FALSE; /* set to true whenever joystick moves */
+
+	signal(SIGINT, int_handler);
+
+	fd_uart = open("/dev/ttyS2", O_RDWR | O_NONBLOCK);
+	if (fd_uart < 0) {
+		perror("/dev/ttyS2");
+		exit(-1);
+	}
+	fd_spi = open("/dev/spimega328", O_RDONLY);
+	if (fd_spi < 0) {
+		perror("/dev/spimega328");
+		exit(-1);
+	}
+
+	main2(argc,argv);
+	midiInit();	// very important: MIDI_WAIT
+
+	sendProgramChange(fd_uart, currentBank->ID, 0); /* bank A program 1: Grand Piano */
+
+
+	while (keep_running) {
+
+		//fbg_flip(fbg);
+		if (read(fd_uart, &byte, 1) == 1) {
+			if (readMidiMessage(byte, &numOfBytes) == TRUE && joychanged == FALSE) {
+				sendMidiMessage(fd_uart, numOfBytes);
+			}
+		}
+
+		if (read(fd_spi, inputdata, 6) > 0) {
+			translateJoystick(inputdata[JOYX], inputdata[JOYY], &joyx, &joyy);
+			sleepTime = calculateSleepTime(joyx);
+			joychanged = TRUE;
+		} else {
+			if (joyx > 5 && currentBank->index < 127)
+				currentBank->index++;
+			else if (joyx < -5 && currentBank->index > 0)
+				currentBank->index--;
+			else
+				joychanged = FALSE;
+
+			if (joychanged == TRUE) {
+				printf("%d: %s\n", currentBank->index + 1,currentBank->names[currentBank->index]);
+				sendProgramChange(fd_uart, currentBank->ID, currentBank->index);
+				usleep(sleepTime);
+			}
+		}
+	}
+
+	close(fd_uart);
+	close(fd_spi);
+
+	return 0;
+}
 
